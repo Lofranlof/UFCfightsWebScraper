c52ac709b3d5b12c85660356713e5df1b60f42d3
davle <lofranlof@gmail.com>
2023-08-13 22:26:38 +0300

    Add project files.

---
+++ UFCfightsWebScraper/Program.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Globalization;
+using System.Net.Http;
+
+
+namespace WebScraper
+{ 
+    class Program
+    {
+       // A basic web scraper for the main event of an upcoming UFC tournament
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            String url = "https://www.ufc.com/events";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url).Result;
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var tournamentNameElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='c-hero--full__headline-prefix']");
+            var tournamentName = tournamentNameElement.InnerText.Trim();
+            Console.WriteLine("Tournament: " + tournamentName);
+
+            var mainEventElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='c-hero--full__headline is-large-text']");
+            var mainEvent = System.Web.HttpUtility.HtmlDecode(mainEventElement.InnerText.Trim());
+            Console.WriteLine("Main Event: " + mainEvent);
+            
+        }
+    }
+}
