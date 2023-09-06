bd54391df6d53e71eeddc634b4d07120ccb91b76
Руслан Давлетов <lofranlof@gmail.com>
2023-09-06 14:59:49 +0300

    QoL update
    Added date, time and location info

--- UFCfightsWebScraper/Program.cs
+++ UFCfightsWebScraper/Program.cs
@@ -20,20 +20,28 @@ namespace WebScraper
 
             var tournamentNameElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='c-hero--full__headline-prefix']");
             var tournamentName = tournamentNameElement.InnerText.Trim();
-            Console.WriteLine("Tournament: " + tournamentName);
+            Console.WriteLine("Tournament:\t" + tournamentName);
 
             var mainEventElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='c-hero--full__headline is-large-text']"); //Will be null during UFC Fight Nights
-            if (mainEventElement != null )
-            {
+            if (mainEventElement != null ) {
                 var mainEvent = System.Web.HttpUtility.HtmlDecode(mainEventElement.InnerText.Trim());
-                Console.WriteLine("Main Event: " + mainEvent);
+                Console.WriteLine("Main Event:\t" + mainEvent);
             }
-            else
-            {
+            else {
                 mainEventElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='c-hero--full__headline is-medium-text']");
                 var mainEvent = System.Web.HttpUtility.HtmlDecode(mainEventElement.InnerText.Trim());
-                Console.WriteLine("Main Event: " + mainEvent);
+                Console.WriteLine("Main Event:\t" + mainEvent);
             }
+
+            var additionalInfoElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='c-card-event--result__date tz-change-data']");
+            var additionalInfo = additionalInfoElement.InnerText.Replace("/ Основной кард", "").Trim().Trim();
+
+            var locationInfoElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='field field--name-location field--type-address field--label-hidden field__item']");
+            var locationInfo = locationInfoElement.InnerText.Replace("\n", " ").Trim();
+
+
+            Console.WriteLine("Date & Time:\t" + additionalInfo);
+            Console.WriteLine("Location:\t" + locationInfo + '\n' + '\n');
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
         }
